88b78857010911c28dc09f012beb7e6c96b778c2
danhfa1999 <danh260199@gmail.com>
2022-11-13 19:04:27 +0700

    chore:update

--- src/Component/Remind.jsx
+++ src/Component/Remind.jsx
@@ -5,8 +5,8 @@ function Remind() {
     <div className="absolute pt-12 bottom-4 right-4 animate-pulse hidden lg:block">
       <div className="px-12 py-10 text-white button">
         <small>Resiter or use default account ❗</small>
-        <div>Account:amazon@gmail.com</div>
-        <div>Password:amazon</div>
+        <div>Account:root@dev.com</div>
+        <div>Password:root1999</div>
       </div>
     </div>
   );
